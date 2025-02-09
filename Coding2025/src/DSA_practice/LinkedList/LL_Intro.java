b426d1f7a7c1ae30b88617ce6f35b899a83c2556
Ritik Kushwaha <ritikkushwahacoding@gmail.com>
2025-02-09 15:16:11 +0530

    this is final file

--- Coding2025/src/DSA_practice/LinkedList/LL_Intro.java
+++ Coding2025/src/DSA_practice/LinkedList/LL_Intro.java
@@ -2,8 +2,7 @@ package DSA_practice.LinkedList;
 
 public class LL_Intro {
     public static void main(String[] args){
-        System.out.println("chaliye shure krte hain");
-
+        System.out.println("Lets begin with the Linked List");
     }
 }
 
