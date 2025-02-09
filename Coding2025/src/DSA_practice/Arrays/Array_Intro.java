8c7b1798ea7402e595a1e51b9f4c60bd0965ec27
Ritik Kushwaha <ritikkushwahacoding@gmail.com>
2025-02-09 12:22:20 +0530

    lets push first

--- Coding2025/src/DSA_practice/Arrays/Array_Intro.java
+++ Coding2025/src/DSA_practice/Arrays/Array_Intro.java
@@ -4,6 +4,7 @@ public class Array_Intro {
     public static void main(String[] args){
         System.out.println("Hello Array");
         int[] arr={1,2,3,4,5,6,7,8,9};
+        System.out.println("Print Array");
         for(int i=0;i<arr.length;i++){
             System.out.println(arr[i]);
         }
