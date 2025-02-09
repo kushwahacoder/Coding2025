4b01a7a23e54c98169f51776a73b947c62f3c85f
Ritik Kushwaha <ritikkushwahacoding@gmail.com>
2025-02-09 15:05:34 +0530

    change in LL

--- Coding2025/src/DSA_practice/LinkedList/LL_Intro.java
+++ Coding2025/src/DSA_practice/LinkedList/LL_Intro.java
@@ -2,7 +2,8 @@ package DSA_practice.LinkedList;
 
 public class LL_Intro {
     public static void main(String[] args){
-        System.out.println("Lets begin with the Linked List");
+        System.out.println("chaliye shure krte hain");
+
     }
 }
 
